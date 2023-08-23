4d5b024f0d691497a0bfe5beeb9bafdc17cb2414
DevBigEazi <adesholatajudeen1@gmail.com>
2023-08-23 07:58:46 +0100

    npm updated

--- src/index.js
+++ src/index.js
@@ -1,67 +1,68 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
-app.use(bodyParser.json())
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({
-    extended:true
-}))
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-mongoose.connect('process.env.MONGODB_URL || mongodb://localhost:27017/form-data',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 let db = mongoose.connection;
 
-db.on('error',()=>console.log("Error in Connecting to Database"));
-db.once('open',()=>console.log("Connected to Database || No Error"))
+db.on("error", () => console.log("Error in Connecting to Database"));
+db.once("open", () => console.log("Connected to Database || No Error"));
 
-app.post("/sign_up",(req,res)=>{
-    let firstname = req.body.firstname;
-    let lastname = req.body.lastname;
-    let gender = req.body.gender;
-    let dob = req.body.dob;
-    let education = req.body.education;
-    let email = req.body.email;
-    let phoneNumber = req.body.phoneNumber;
-    let nationality = req.body.nationality;
-    let state = req.body.state;
+app.post("/sign_up", (req, res) => {
+  let firstname = req.body.firstname;
+  let lastname = req.body.lastname;
+  let gender = req.body.gender;
+  let dob = req.body.dob;
+  let education = req.body.education;
+  let email = req.body.email;
+  let phoneNumber = req.body.phoneNumber;
+  let nationality = req.body.nationality;
+  let state = req.body.state;
 
-    let data = {
-        "firstname": firstname,
-        "lastname": lastname,
-        "gender": gender,
-        "dob": dob,
-        "education": education,
-        "email" : email,
-        "phoneNumber": phoneNumber,
-        "nationality": nationality,
-        "state" : state
+  let data = {
+    firstname: firstname,
+    lastname: lastname,
+    gender: gender,
+    dob: dob,
+    education: education,
+    email: email,
+    phoneNumber: phoneNumber,
+    nationality: nationality,
+    state: state,
+  };
+
+  db.collection("users").insertOne(data, (err, collection) => {
+    if (err) {
+      throw err;
     }
+    console.log("Record Inserted Successfully");
+  });
 
-    db.collection('users').insertOne(data,(err,collection)=>{
-        if(err){
-            throw err;
-        }
-        console.log("Record Inserted Successfully");
-    });
+  return res.redirect("signup_success.html");
+});
 
-    return res.redirect('signup_success.html')
-
-})
-
-
-app.get("/",(req,res)=>{
+app
+  .get("/", (req, res) => {
     res.set({
-        "Allow-access-Allow-Origin": '*'
-    })
-    return res.redirect('index.html');
-}).listen(port);
-
+      "Allow-access-Allow-Origin": "*",
+    });
+    return res.redirect("index.html");
+  })
+  .listen(port);
 
 console.log("App running on PORT 3000");
